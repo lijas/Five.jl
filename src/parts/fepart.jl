1bc843195b9368b27a5f49eabc34ba30ce21511f
Elias <elias.isak.borjesson@gmail.com>
2021-03-12 14:52:24 +0100

    add vtk plot for cohesive zoens

--- src/parts/fepart.jl
+++ src/parts/fepart.jl
@@ -95,8 +95,8 @@ end
 
 
 function init_part!(part::Part, dh::JuAFEM.AbstractDofHandler)
-
     celltype = typeof(dh.grid.cells[first(part.cellset)])
+    
     next_node_id = 1
     for cellid in part.cellset#CellIterator2(dh, part.element, part.cellset)
         cell = dh.grid.cells[cellid]
@@ -284,39 +284,6 @@ function get_vtk_displacements(dh::JuAFEM.AbstractDofHandler, part::Part{dim,T},
     end
     
     return node_coords
-end
-
-function init_part!(part::Part{dim,T,<:CohesiveElement}, dh::JuAFEM.AbstractDofHandler) where {dim,T}
-    return nothing
-    #=@assert(dim==2)
-    celltype = JuAFEM.VTKCellTypes.VTK_LINE
-    
-    next_node_id = 1
-    @show part.cellset
-    for cell in CellIterator(dh, part.cellset)
-        new_ids = Int[]
-        for nodeid in cell.nodes
-            if !haskey(part.vtkexport.nodeid_mapper, nodeid)
-                part.vtkexport.nodeid_mapper[nodeid] = next_node_id
-                push!(new_ids, next_node_id)
-                next_node_id += 1
-                push!(part.vtkexport.vtknodes, dh.grid.nodes[nodeid].x)
-            else
-                _new_id = part.vtkexport.nodeid_mapper[nodeid]
-                push!(new_ids, _new_id)
-            end
-        end
-        #@show cell.nodes
-        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[1:2]))
-        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[2:3]))
-        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[3:4]))
-        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[[4,1]]))
-        
-    end=#
-end
-
-function get_vtk_displacements(dh::JuAFEM.AbstractDofHandler, part::Part{dim,T,<:CohesiveElement}, state::StateVariables) where {dim,T}
-    return Vec{dim,T}[]
 end
 
 function get_vtk_celldata(part::FEPart, output::VTKCellOutput{<:MaterialStateOutput}, state::StateVariables{T}, globaldata) where T
--- src/utils/cohesive_element_utils.jl
+++ src/utils/cohesive_element_utils.jl
@@ -49,6 +49,8 @@ JuAFEM.faces(c::CohesiveCell{2,N,2}) where N    = ((c.nodes[1], c.nodes[2]), (c.
 JuAFEM.default_interpolation(::Type{CohesiveCell{2,4,2}}) = CohesiveZoneInterpolation(Lagrange{1,RefCube,1}())
 JuAFEM.default_interpolation(::Type{CohesiveCell{2,6,2}}) = CohesiveZoneInterpolation(Lagrange{1,RefCube,2}())
 
+JuAFEM.cell_to_vtkcell(::Type{CohesiveCell{2,4,2}}) = JuAFEM.VTKCellTypes.VTK_QUAD
+
 """
     SurfaceVectorValues
 
@@ -175,4 +177,42 @@ function JuAFEM.function_value(fe_v::SurfaceVectorValues{dim,dim_s}, q_point::In
     end
     return val
 end
+
+
+
+#Fe part
+
+#=
+function init_part!(part::Part{dim,T,<:CohesiveElement}, dh::JuAFEM.AbstractDofHandler) where {dim,T}
+    return nothing
+    #=@assert(dim==2)
+    celltype = JuAFEM.VTKCellTypes.VTK_LINE
+    
+    next_node_id = 1
+    @show part.cellset
+    for cell in CellIterator(dh, part.cellset)
+        new_ids = Int[]
+        for nodeid in cell.nodes
+            if !haskey(part.vtkexport.nodeid_mapper, nodeid)
+                part.vtkexport.nodeid_mapper[nodeid] = next_node_id
+                push!(new_ids, next_node_id)
+                next_node_id += 1
+                push!(part.vtkexport.vtknodes, dh.grid.nodes[nodeid].x)
+            else
+                _new_id = part.vtkexport.nodeid_mapper[nodeid]
+                push!(new_ids, _new_id)
+            end
+        end
+        #@show cell.nodes
+        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[1:2]))
+        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[2:3]))
+        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[3:4]))
+        push!(part.vtkexport.vtkcells, MeshCell(celltype, new_ids[[4,1]]))
+        
+    end=#
+end
+
+function get_vtk_displacements(dh::JuAFEM.AbstractDofHandler, part::Part{dim,T,<:CohesiveElement}, state::StateVariables) where {dim,T}
+    return Vec{dim,T}[]
+end=#
 
