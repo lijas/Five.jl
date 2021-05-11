4d757d76e7e67b565a49895919d28b86c6696715
Elias <elias.isak.borjesson@gmail.com>
2021-05-11 09:08:30 +0200

    add stuff for publishing docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,8 @@ makedocs(
     modules = [Five]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/lijas/Five.jl.git",
+    push_preview=true,
+    devbranch = "master"
+)
